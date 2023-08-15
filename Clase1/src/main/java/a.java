4b042b1c772bba0a5224903305a2b809df2c5132
an6147 <an6147@gmail.com>
2023-08-15 13:33:23 -0400

    First JAVA contact

---
+++ Clase1/src/main/java/a.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Cempai
+ */
+class a {
+    public static void main(String[] args) {
+        String a = "error";
+        String b = "023";
+        System.out.println(a + Integer.parseInt(b));
+    }
+}
---
+++ Clase1/src/main/java/com/mycompany/clase1/Clase1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clase1;
+
+/**
+ *
+ * @author Cempai
+ */
+public class Clase1 {
+
+    public static void main(String args[]) {
+        System.out.println("Calculo del area de un triangulo:");
+        double base = 30.4;
+        double high = 5.0;
+        double areaTriangle = base*high;
+        System.out.println("La base es:" + base + "\nLa altura es:" + high);
+        System.out.println("El area es:" + areaTriangle);
+     }
+}
